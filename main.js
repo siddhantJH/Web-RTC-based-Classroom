3e07bfb61885f31a93e3ef7573c02d48aa5bfe1a
Siddhant jha <40361603+siddhantJH@users.noreply.github.com>
2024-03-16 06:50:44 +0000

    new

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 const colors = document.querySelectorAll(".col");
-const toolBox=document.querySelector(".tools")
-const tools=toolBox.childNodes
+const canvas = document.querySelector(".drawing-pannel");
+const ctx = canvas.getContext('2d');
+const canvasOffsetX=canvas.offsetLeft;
+const canvasOffsetY=canvas.offsetTop;
 
+let isPainting=false;
+let lineWidth=5;
+let startX;
+let startY;
 
-colors.forEach(color => {
-    color.addEventListener("click", function(e) {
-      console.log(e)
-    });
+canvas.addEventListener("mousedown", function(e) {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
 });
 
 
-tools.forEach(tool=>{
-    tool.addEventListener("click",function(e){
-        console.log(e)
-    })
-})
+canvas.addEventListener('mousemove',draw)
 
 
+function draw(e) {
+    console.log('draw');
+    if (! isPainting) return;
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+}
 
 
+function stopDrawing()
+{
+    console.log('stopDrawing');
+    isPainting=false;
+}
